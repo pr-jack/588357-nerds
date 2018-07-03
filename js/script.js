fc7b6ae9e4196bfc6d01939944a9679a671d762b
pr-jack <prevgenij@gmail.com>
2018-07-03 10:38:32 +0200

    Оживление

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var link = document.querySelector(".btn-contacts");
+
+var popup = document.querySelector(".modal-feedback");
+
+var close = document.querySelector(".modal-close");
+
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=email]");
+
+var form = popup.querySelector("form");
+
+var isStorageSupport = true;
+var storage = "";
+  
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+      
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+    if (storage) {
+      username.value = storage;
+      email.focus();
+      } else {
+        username.focus();
+      }    
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");     
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value) {
+  evt.preventDefault();
+  console.log("Нужно ввести логин и пароль");
+  }else {
+    if (isStorageSupport) {
+      localStorage.setItem("username", username.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
